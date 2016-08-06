5211805aaa574696f612dde320bbf93132f6b13f
FlavioMoreira <flavio@janus>
2016-08-06 18:09:10 +1000

    updating atom to a local dir

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-cd ~/SourceCode/atom/
+cd ~/Develop/atom/
 git checkout master
 git pull
 git fetch -p
 git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
 script/build
-sudo script/grunt install
-
+sript/grunt install --install-dir /home/flavio/Local/stow/atom
